cfa1755b2b4df1f6d2f11c14129faf5ea2b43d95
Sly Gryphon <sgryphon@computer.org>
2022-07-13 23:16:21 +1000

    Script to load models

---
+++ smart-factory/factory-line-twin/import-model.ps1
@@ -0,0 +1,42 @@
+#!/usr/bin/env pwsh
+
+<# .SYNOPSIS
+  Imports the factory ontology model
+#>
+[CmdletBinding()]
+param (
+    ## Deployment environment, e.g. Prod, Dev, QA, Stage, Test.
+    [string]$Environment = 'Dev',
+    ## Identifier for the organisation (or subscription) to make global names unique.
+    [string]$OrgId = $ENV:DEPLOY_ORGID ?? "0x$((az account show --query id --output tsv).Substring(0,4))"
+)
+
+$ErrorActionPreference="Stop"
+
+$SubscriptionId = $(az account show --query id --output tsv)
+Write-Verbose "Using subscription ID $SubscriptionId"
+
+$appName = 'iotcore'
+$dtName = "dt-$appName-$OrgId-$Environment".ToLowerInvariant()
+
+Write-Verbose "Loading factory ontology models to $dtName"
+
+Write-Verbose "Geo ontology models"
+
+az dt model create --dt-name $dtName --from-directory (Join-Path $PSScriptRoot 'Ontology/Geo') 
+
+Write-Verbose "SAREF Core ontology models"
+
+az dt model create --dt-name $dtName --from-directory (Join-Path $PSScriptRoot 'Ontology/Core') 
+
+Write-Verbose "SAREF Building ontology models"
+
+az dt model create --dt-name $dtName --from-directory (Join-Path $PSScriptRoot 'Ontology/Building') 
+
+Write-Verbose "SAREF Manufacturing ontology models"
+
+az dt model create --dt-name $dtName --from-directory (Join-Path $PSScriptRoot 'Ontology/Manufacturing') 
+
+Write-Verbose "Chocolate factory ontology models"
+
+az dt model create --dt-name $dtName --from-directory (Join-Path $PSScriptRoot 'Ontology/Factory') 
