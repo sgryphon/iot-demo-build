3faa7a0d6411609a371d189a02159af9eae79742
Sly Gryphon <sly.gryphon@purple.telstra.com>
2022-09-15 14:12:42 +1000

    Enable forwarded headers for caddy proxy

--- telstra-digital-twins/demo-data/tdt-simulator/Program.cs
+++ telstra-digital-twins/demo-data/tdt-simulator/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +8,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders =
+        ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+});
 
 var app = builder.Build();
+
+app.UseForwardedHeaders();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
