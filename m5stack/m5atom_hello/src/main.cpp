33bee26a923b325e28337d75728b389aced86ecd
Sly Gryphon <sgryphon@computer.org>
2022-09-20 15:35:09 +1000

    Update to M5 Unified

--- m5stack/m5atom_hello/src/main.cpp
+++ m5stack/m5atom_hello/src/main.cpp
@@ -1,27 +1,35 @@
-#include <M5Atom.h>
+#include <M5Unified.h>
+#include <FastLED.h>
 #include <Arduino.h>
 
 static const char *TAG = "Hello";
 
+#define NUM_LEDS 1
+#define DATA_PIN 27
+
 bool led = true;
+CRGB leds[NUM_LEDS];
 
 void setup() {
-  M5.begin(true, true, true);
+  M5.begin();
   delay(10);
   Serial.println("Atom started");
-  M5.dis.fillpix(CRGB::Green); 
+  FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
+  leds[0] = CRGB::Green;
+  FastLED.show();
 }
 
 void loop() {
   M5.update();
-  if (M5.Btn.wasPressed()) {
+  if (M5.BtnA.wasPressed()) {
     ESP_LOGD(TAG, "Button pressed");
     led = !led;
     Serial.printf("Button was pressed %s\n", led ? "on" : "off");
     if (led) {
-      M5.dis.fillpix(CRGB::Blue);
+      leds[0] = CRGB::Blue;
     } else {
-      M5.dis.clear();
+      leds[0] = CRGB::Black;
     }
+    FastLED.show();
   }
 }
