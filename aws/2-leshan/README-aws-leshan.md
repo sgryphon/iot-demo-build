3038daa3b6e60c5c397a4a84536d8f2f17875e3d
Sly Gryphon <sgryphon@computer.org>
2023-06-05 09:57:58 +1000

    wip - adding elastic IP and DNS

--- aws/2-leshan/README-aws-leshan.md
+++ aws/2-leshan/README-aws-leshan.md
@@ -6,8 +6,6 @@ Contents
 
 Creates an Elastic Compute (EC) machine, running a Leshan LwM2M server.
 
-
-
 Requirements
 ------------
 
@@ -17,6 +15,43 @@ Requirements
 
 Dependencies:
 * Uses the networks created in `1-core`
+
+Bring your own DNS
+------------------
+
+AWS will automatically generate a DNS named for public IPv4 addresses (based on the IP address),
+but not for IPv6.
+
+To test IPv6 with security, e.g. certificates, you need to provide your own DNS names, assign the IP addresses, and use them when creating the machine.
+
+Once the Virtual Private Cloud network is created, an IPv6 range is allocated to it. You can assign addresses from this range as needed, so we pick an address to use from the dual stack public network. Because you get allocated an entire `/56` range per VPC we can easily assign as many addresses as we need.
+
+In contrast, IPv4 addresses are scarce, and we need to pre-allocate a specific Elastic IP (EIP) address, to know the value. Usage of IPv4 addresses is limited to five (5) EIPs per region.
+
+We can use the first script to create the Elastic IP, and then output the picked IPv6 address along with the EIP.
+
+```powershell
+.\deploy-001-elastic-ip.ps1
+```
+
+This will end with output like the following:
+
+```
+Assign IPv4 DNS (based on EIP eipalloc-0b5126219f9e0e830)
+IPv4 address: 13.211.73.98
+
+Assign IPv6 DNS (based on subnet core-network-dev-public-subnet-02)
+IPv6 subnet: 2406:da1c:ab1:2901::/64
+IPv6 address: 2406:da1c:ab1:2901::100d
+Internal private IPv4 address: 10.0.1.13
+```
+
+You can use the output IP addresses to configure host names in your DNS, e.g.:
+
+```
+lwm2mdev01.demo AAAA 2406:da1c:ab1:2901::100d
+lwm2mdev01v4.demo A 13.211.73.98
+```
 
 Running scripts
 ---------------
@@ -38,31 +73,34 @@ Log in, if needed (e.g. configuration was previously set up)
 aws sso login
 ```
 
-Then run all the scripts:
+Set the configuration values to the DNS names you created (replace the same ones below),
+and run the script to create the server:
 
 ```powershell
+$ENV:DEPLOY_HOST_IPV6 = 'lwm2mdev01.demo.gamertheory.net'
+$ENV:DEPLOY_HOST_IPV4 = 'lwm2mdev01v4.demo.gamertheory.net'
 ./deploy-002-leshan.ps1
 ```
 
 Accessing
 ---------
 
+You can access the machine via IPv6 SSH:
+
 ```powershell
-$instance = (aws ec2 describe-instances --filters Name=key-name,Values=leshan-dev-key Name=instance-state-name,Values=running | ConvertFrom-Json).Reservations | Select -First 1
-$instance.Instances.Ipv6Address
-$instance.Instances.PublicIpAddress
-``` 
+ssh -i ~/.ssh/leshan-dev-key.pem ec2-user@lwm2mdev01.demo.gamertheory.net
+```
 
 You can access the machine via IPv6 SSH, to the IP address:
 
 ```powershell
-ssh -i ~/.ssh/leshan-dev-key.pem ec2-user@2406:da1c:ab1:2901:93fc:9eed:d9f7:bc6e
+ssh -i ~/.ssh/leshan-dev-key.pem ec2-user@2406:da1c:ab1:2901::100d
 ```
 
 Or via IPv4 to the auto-generated DNS name (based on the IPv4 address):
 
 ```powershell
-ssh -i ~/.ssh/leshan-dev-key.pem ec2-user@ec2-13-236-85-52.ap-southeast-2.compute.amazonaws.com
+ssh -i ~/.ssh/leshan-dev-key.pem ec2-user@ec2-13-211-73-98.ap-southeast-2.compute.amazonaws.com
 ```
 
 You can also copy the keys to another machine to use. If you copy to a Linux machine, make sure
--- aws/2-leshan/deploy-001-elastic-ip.ps1
+++ aws/2-leshan/deploy-001-elastic-ip.ps1
@@ -1,16 +1,9 @@
 #!/usr/bin/env pwsh
 
 <# .SYNOPSIS
-  Deploy landing virtual private cloud (vpc) network.
+  Deploy public IP address to use for the Leshan server
 
 .NOTES
-  This creates a core network in your AWS account
-
-  The network is dual stack and you should pass in -UlaGlobalId and subnet IDs
-  for the IPv6 Unique Local Address network, or if you do not they will be 
-  generated based on a consistent unique hash of the subscription ID.
-  For more information on ULAs see https://en.wikipedia.org/wiki/Unique_local_address
-
   Running these scripts requires the following to be installed:
   * PowerShell, https://github.com/PowerShell/PowerShell
   * AWS CLI, https://docs.aws.amazon.com/cli/index.html
@@ -44,16 +37,12 @@ $Environment = $ENV:DEPLOY_ENVIRONMENT ?? 'Dev'
 
 $ErrorActionPreference="Stop"
 
-$stackTemplateFile = Join-Path $PSScriptRoot "cf-002-leshan.yaml"
-$stackName = "leshan-${Environment}".ToLowerInvariant()
+$stackTemplateFile = Join-Path $PSScriptRoot "cf-001-elastic-ip.yaml"
+$stackName = "leshan-ip-${Environment}".ToLowerInvariant()
 
 $networkStackName = "core-network-${Environment}".ToLowerInvariant()
 $vpcName = "$networkStackName-01".ToLowerInvariant()
-$publicSubnet01Name = "$networkStackName-public-subnet-01".ToLowerInvariant()
 $publicSubnet02Name = "$networkStackName-public-subnet-02".ToLowerInvariant()
-
-$keyName = "leshan-${Environment}-key".ToLowerInvariant()
-$sshFolder = "~/.ssh"
 
 # Check if profile and region environment variables are set
 if (-not $ENV:AWS_PROFILE) {
@@ -74,13 +63,6 @@ $vpc = aws ec2 describe-vpcs `
     | Select-Object -ExpandProperty Vpcs `
     | Select-Object -First 1
 
-Write-Verbose "Getting public IPv6 subnet $publicSubnet01Name" 
-$ipv6Subnet = aws ec2 describe-subnets `
-  --filters "Name=tag:Name,Values=$publicSubnet01Name" "Name=vpc-id,Values=$($vpc.VpcId)" `
-  | ConvertFrom-Json `
-  | Select-Object -ExpandProperty Subnets `
-  | Select-Object -First 1   
-
 Write-Verbose "Getting public dual stack subnet $publicSubnet02Name" 
 $dualStackSubnet = aws ec2 describe-subnets `
   --filters "Name=tag:Name,Values=$publicSubnet02Name" "Name=vpc-id,Values=$($vpc.VpcId)" `
@@ -97,21 +79,9 @@ $dsIpv4Prefix = $dualStackSubnet.CidrBlock
 $privateIpv4Suffix = [int]"0x$($PrivateIpSuffix.Substring($PrivateIpSuffix.Length -2))"
 $dsIpv4Address = "$($dsIpv4Prefix.Substring(0, $dsIpv4Prefix.Length - 4))$privateIpv4Suffix"
 
-Write-Verbose "Using addresses $dsIpv6Address, $dsIpv4Address" 
+# Creating Elastic IP
 
-$keyPath ="$sshFolder/$keyName.pem"
-if (Test-Path $keyPath) {
-  Write-Verbose "Using key pair $keyName found at path: $keyPath"
-} else {
-  Write-Verbose "Generate key pair $keyName and saving to: $keyPath"
-  aws ec2 create-key-pair --key-name $keyName --query 'KeyMaterial' --output text | Out-File $keyPath
-  if (!$?) { throw "AWS CLI returned error: $LastExitCode" }
-}
-
-Write-Verbose "Deploying cloud formation to create server stack $stackName"
-
-$instanceType = 't3.micro'
-$amiImageId = '/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-6.1-x86_64'
+Write-Verbose "Deploying cloud formation to create EIP stack $stackName"
 
 aws cloudformation deploy `
     --template-file $stackTemplateFile `
@@ -119,25 +89,28 @@ aws cloudformation deploy `
     --tags `
       "BusinessUnit=IoT" `
       "DataClassification=Non-business" `
-      "Environment=$Environment" `
-    --parameter-overrides "InstanceType=$instanceType" `
-      "KeyName=$keyName" `
-      "AmiImageId=$amiImageId" `
-      "VPCId=$($vpc.VpcId)" `
-      "Ipv6SubnetId=$($ipv6Subnet.SubnetId)" `
-      "DualStackSubnetId=$($dualStackSubnet.SubnetId)" `
-      "DualStackIpv6Address=$dsIpv6Address" `
-      "DualStackIpv4Address=$dsIpv4Address"
+      "Environment=$Environment"
 if (!$?) { throw "AWS CLI returned error: $LastExitCode" }
 
-# aws cloudformation delete-stack --stack-name 'leshan-dev'
+# aws cloudformation delete-stack --stack-name 'leshan-ip-dev'
 
 # Retrieve the output values
 $stackOutputs = aws cloudformation describe-stacks `
-    --stack-name $networkStackName `
+    --stack-name $stackName `
     | ConvertFrom-Json `
     | Select-Object -ExpandProperty Stacks
 
 Write-Host "Stack outputs:`n$($stackOutputs | ConvertTo-Json -Depth 10)"
 
-Write-Host "To connect: ssh -i $keyPath <host-dns-name>"
+$eipAddressName = "$stackName-address"
+$eipAddress = aws cloudformation list-exports --query "Exports[?Name=='$eipAddressName'].Value" --output text
+$eipAllocationIdName = "$stackName-allocation-id"
+$eipAllocationId = aws cloudformation list-exports --query "Exports[?Name=='$eipAllocationIdName'].Value" --output text
+
+Write-Host "`nAssign IPv4 DNS (based on EIP $eipAllocationId)" -ForegroundColor Green
+Write-Host "IPv4 address: $eipAddress" 
+
+Write-Host "`nAssign IPv6 DNS (based on subnet $publicSubnet02Name)" -ForegroundColor Green
+Write-Host "IPv6 subnet: $dsIpv6Prefix" 
+Write-Host "IPv6 address: $dsIpv6Address" 
+Write-Host "Internal private IPv4 address: $dsIpv4Address" 
