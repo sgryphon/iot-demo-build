f7501b87e5c854f3acab30d46883133db7066131
Sly Gryphon <sgryphon@computer.org>
2023-09-04 12:17:13 +1000

    IPv6 network deploy

--- aws/aws-landing/bin/aws-landing.ts
+++ aws/aws-landing/bin/aws-landing.ts
@@ -2,20 +2,35 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { AwsLandingStack } from '../lib/aws-landing-stack';
+import { IpAddresses } from 'aws-cdk-lib/aws-ec2';
 
 const app = new cdk.App();
-new AwsLandingStack(app, 'AwsLandingStack', {
+new AwsLandingStack(app, 'AwsLandingStack-dev', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  ipv4PrivateAddresses: IpAddresses.cidr('10.1.0.0/21'),
+  environment: 'Dev',
+});
+
+new AwsLandingStack(app, 'AwsLandingStack-test1', {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  ipv4PrivateAddresses: IpAddresses.cidr('10.96.0.0/21'),
+  environment: 'Test',
+});
+
+new AwsLandingStack(app, 'AwsLandingStack-test2', {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  ipv4PrivateAddresses: IpAddresses.cidr('10.97.0.0/21'),
+  environment: 'Test',
 });
--- aws/aws-landing/lib/aws-landing-stack.ts
+++ aws/aws-landing/lib/aws-landing-stack.ts
@@ -1,16 +1,28 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { NetworkLayer } from './network-layer';
+import { IIpAddresses } from 'aws-cdk-lib/aws-ec2';
+import { CfnOutput, Tags } from 'aws-cdk-lib';
 
 export class AwsLandingStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: AwsLandingStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const networkLayer = new NetworkLayer(this, "Network", {
+      ipv4PrivateAddresses:  props?.ipv4PrivateAddresses,
+      maxAzs: 2
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsLandingQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    Tags.of(this).add('Owner', 'IoT Lab');
+    Tags.of(this).add('Classification', 'Confidential');
+
+    new CfnOutput(this, 'VpcId', { value: networkLayer.vpc.vpcId });
+    new CfnOutput(this, 'PublicSubnetIds', { value: networkLayer.vpc.publicSubnets.map(x => x.subnetId).join(',') });
+    new CfnOutput(this, 'PrivateSubnetIds', { value: networkLayer.vpc.privateSubnets.map(x => x.subnetId).join(',') });
   }
+}
+
+export interface AwsLandingStackProps extends cdk.StackProps {
+  readonly environment?: string;
+  readonly ipv4PrivateAddresses?: IIpAddresses;
 }
